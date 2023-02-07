d7948409e173b995539fdc00a2bd128a328e7191
greglabo78 <gregoryobigodwin@yahoo.com>
2023-02-07 12:25:05 +0000

    Add event listeners for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,68 @@
+const form = document.getElementById("form");
+
+// Wait for the DOM to finish loading before loading before running the ToDo application
+
+//Get the button Elements and add event listners to them
+
+ 
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    bindButtonEvents();
+
+});
+
+function bindButtonEvents() {
+
+    const addTaskButton = document.getElementById("add-task");
+
+    const clearCompletedTasksButton = document.getElementById("clear-completed-tasks");
+
+    const clearAllTasksButton = document.getElementById("clear-all-tasks");
+
+   
+
+    addTaskButton.addEventListener("click", function(el) {
+
+       el.preventDefault();
+
+       createTask();      
+
+    });
+
+   
+
+    clearCompletedTasksButton.addEventListener("click", function(el) {       
+
+       el.preventDefault();
+
+       clearCompletedTasks();
+
+    });
+
+   
+
+    clearAllTasksButton.addEventListener("click", function(el) {       
+
+       el.preventDefault();
+
+       clearAllTasks();
+
+    });
+
+}
+
+   
+
+
+
+
+
+
+
+
+
+
 function createTask() {
 
 }
@@ -19,5 +84,5 @@ function clearCompletedTasks() {
 }
 
 function clearAllTasks() {
-    
+
 }
