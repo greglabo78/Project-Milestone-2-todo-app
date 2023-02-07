4163b9291fc0356fca7922bf95adb79082bdf2b6
greglabo78 <gregoryobigodwin@yahoo.com>
2023-02-07 12:13:18 +0000

    Add structures for Todo Application functions

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+function createTask() {
+
+}
+
+function completedTask() {
+
+}
+
+function editTask() {
+
+}
+
+function deleteTask() {
+
+}
+
+function clearCompletedTasks() {
+
+}
+
+function clearAllTasks() {
+    
+}
